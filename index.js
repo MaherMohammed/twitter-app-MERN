92d3e743cffbea13440abcc28473f927f25a72e7
MaherMohammed <maher.mohammed10.97@gmail.com>
2021-05-11 01:18:06 +0200

    setting up server and database

---
+++ index.js
@@ -0,0 +1,28 @@
+//password: A75cQTINGti3rJ7E
+// database connection : mongodb+srv://maher:<password>@twitter-app.lsdk0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+const db = require('./config/keys').mongoURI;
+
+mongoose.connect(db,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(()=> console.log('CONNECTED TO DATABASE SUCCESSFULLY !'))
+.catch(err => console.log(err));
+
+app.get('/',(req,res)=>{
+    res.send('hello world!');
+})
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=>{
+    console.log(`SERVER RUNNING ON POSRT ${PORT}`);
+})
