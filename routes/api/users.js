911d3b11c7acddf3833ba283fcc389239853d0c9
MaherMohammed <maher.mohammed10.97@gmail.com>
2021-05-14 09:28:28 +0200

    login user before jsonwebtoken

--- routes/api/users.js
+++ routes/api/users.js
@@ -9,29 +9,73 @@ router.get('/testUsers', (req,res)=>{
     })
 })
 
-
+//register user
 router.post('/register', (req,res)=>{
-    // check if there is already a user with this email
-    User.findOne({email: req.body.email})
-    .then(user =>{
+    User.findOne({
+        email: req.body.email
+    })
+    .then((user)=>{
         if (user) {
-            return res.status.apply(400).json({
-                email: "A user has already registered with this email"
-            })
-        }
-        else{
-            //create new user and add it to the DB
+            return res.status(400).json({msg:'A user is already registered with this email'});
+        } else {
+            
+            //create new user
             const newUser = new User({
                 handle: req.body.handle,
                 email: req.body.email,
                 password: req.body.password
             })
 
-            // hash the password
+            //hash the password
+
+            bcrypt.genSalt((err,salt)=>{
+                bcrypt.hash(newUser.password, salt,(err,hash)=>{
+                    newUser.password = hash;
+                    newUser.save()
+                    .then((user)=>{
+                        res.json(user)
+                    })
+                    .catch(err => console.log(err));
+
+                })
+            })
+
         }
     })
-    
 })
 
+
+//login user
+
+router.post('/login', (req,res)=>{
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({
+        email
+    })
+    .then(user => {
+        if (!user) {
+            return res.status(404).json({
+                email: 'This email is not registered'
+            })
+        }
+
+        // compare password
+
+        bcrypt.compare(password, user.password)
+        .then(isMatch =>{
+            if (isMatch) {
+                return res.status(200).json({
+                    msg: 'Success!!'
+                })
+            } else {
+                return res.status(400).json({
+                    password: 'Incorrect Password!'
+                })
+            }
+        })
+    })
+})
 
 module.exports = router;
