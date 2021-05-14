cbf486248537509bcc2dae354bbb86f0a74811f1
MaherMohammed <maher.mohammed10.97@gmail.com>
2021-05-14 03:53:31 +0200

    to hash the password

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const User = require('../../models/User');
 
 router.get('/testUsers', (req,res)=>{
     res.json({
         msg: 'Hello Users'
     })
+})
+
+
+router.post('/register', (req,res)=>{
+    // check if there is already a user with this email
+    User.findOne({email: req.body.email})
+    .then(user =>{
+        if (user) {
+            return res.status.apply(400).json({
+                email: "A user has already registered with this email"
+            })
+        }
+        else{
+            //create new user and add it to the DB
+            const newUser = new User({
+                handle: req.body.handle,
+                email: req.body.email,
+                password: req.body.password
+            })
+
+            // hash the password
+        }
+    })
+    
 })
 
 
