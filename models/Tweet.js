c5db3ef77095c53d5fc1c06fd10939a782e5be25
MaherMohammed <maher.mohammed10.97@gmail.com>
2021-05-18 11:58:19 +0200

    tweet model mongoose

---
+++ models/Tweet.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const TweetShema = new Schema({
+    user:{
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    text:{
+        type:String,
+        required:true
+    },
+    date:{
+        type:Date,
+        default:Date.now
+    }
+    
+});
+
+module.exports = Tweet = mongoose.model('Tweet',TweetShema);
