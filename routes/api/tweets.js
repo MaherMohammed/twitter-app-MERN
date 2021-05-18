b3dfe581189c4aed16888f3666591a0d4be4249f
MaherMohammed <maher.mohammed10.97@gmail.com>
2021-05-18 12:03:26 +0200

    tweets router

--- routes/api/tweets.js
+++ routes/api/tweets.js
@@ -1,11 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
 
-router.get('/testTweets', (req,res)=>{
-    res.json({
-        msg: 'Hello Tweets'
-    })
+const Tweet = require('../../models/Tweet');
+const validateTweete = require('../../validation/tweets');
+
+
+router.get('/',(req,res)=>{
+    Tweet.find()
+        .sort({ date: -1 })
+        .then(tweets => res.json(tweets))
+        .catch(err => res.status(404).json({ notweetsfound: 'No tweets found' }));
 })
+
+//search by user id
+router.get('/user/:user_id', (req, res) => {
+    Tweet.find({user: req.params.user_id})
+        .then(tweets => res.json(tweets))
+        .catch(err =>
+            res.status(404).json({ notweetsfound: 'No tweets found from that user' }
+        )
+    );
+});
+
+
+//search for specific tweet
+router.get('/:id', (req, res) => {
+    Tweet.findById(req.params.id)
+        .then(tweet => res.json(tweet))
+        .catch(err =>
+            res.status(404).json({ notweetfound: 'No tweet found with that ID' })
+        );
+});
+
 
 
 module.exports = router;
